a1fd24455b4650204fc4b1d75b3c168264f654de
marthaurion <marthaurion@gmail.com>
2014-08-11 13:09:43 -0500

    added code to download torrent files as they are found

--- nyaa.py
+++ nyaa.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 # from urlparse import urlparse
+# from subprocess import call
 import re
+import os
+import urllib
 
 FOLLOWED_SHOWS = {"Sword Art Online": ["Commie", "HorribleSubs"],
                   "Aldnoah": ["HorribleSubs", "Commie"],
-                  "Majimoji Rurumo": ["HorribleSubs"]}
+                  "Majimoji Rurumo": ["HorribleSubs"],
+                  "Prisma Ilya": ["HorribleSubs", "UTW"]}
 MAX_LOOPS = 10
 
 
@@ -13,26 +17,29 @@ def valid_download(file_name):
     # standard check for null and make sure the file type is mkv
     if file_name is None or ".mkv" not in file_name:
         return False
-    found = False
 
     if "[480p]" in file_name or "[1080p]" in file_name:
         return False
 
     # check a list of followed shows
     for key in FOLLOWED_SHOWS:
-        for show in FOLLOWED_SHOWS[key]:
-            if key in file_name and "["+show+"]" in file_name:
-                found = True
+        for sub in FOLLOWED_SHOWS[key]:
+            sub_test = "["+sub.lower()+"]" in file_name.lower()
+            if key.lower() in file_name.lower() and sub_test:
+                return True
 
-    # this weird style is in case I want to add logic after this
-    if not found:
-        return False
-
-    # check a list of approved fansub groups
-    return True
+    # default to false
+    return False
 
 base_url = "http://www.nyaa.se/?cats=1_37"  # category for english-translated anime
 url2 = "http://www.nyaa.se"  # base url in case the category doesn't work
+
+base_dir = os.getcwd()
+dirname = base_dir + "/torrents/pending/"
+if not os.path.exists(dirname):
+    os.makedirs(dirname)
+
+random_file = urllib.URLopener()
 
 for i in range(1, MAX_LOOPS):
     url = base_url+"&offset="+str(i)
@@ -44,14 +51,17 @@ for i in range(1, MAX_LOOPS):
     #print soup.prettify()
 
     anime = soup.find_all(href=re.compile("view"))
-    to_download = set()
 
     for element in anime:
         dl_link = str(element['href']).replace("?page=view", "?page=download")
         text = element.getText().replace("_", " ")
         if valid_download(text):
             print dl_link, text
-            to_download.add(dl_link)
+            # if we haven't downloaded the files, download them
+            if not os.path.exists(dirname + text):
+                random_file.retrieve(dl_link, dirname + text)
 
     #parsed_link = urlparse(link)
     #base_link = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_link)
+
+#call(["rtorrent", to_download.pop()])
